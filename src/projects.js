2ce4cea7029bb6f8b113ace8e9a6199d152f40dc
Vrushabh Mungelwar <vrushabhmungelwar10@gmail.com>
2022-02-14 23:40:53 +0530

    initial commit

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,37 @@
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { IconButton } from "@mui/material";
 export function Projects() {
-    return (
-        <div>
-            <h2>Projects</h2>
+  return (
+    <div className="project-cont">
+      <h2>Projects</h2>
+
+      <div className="projects">
+        <div className="proj">
+          <h3>Login and SignUp Page</h3>
+          <p>Login,SignUp and forgot password with Email Verification.</p>
+          <h3>Technologies Used: React.js,Node.js,Express.js,Mongodb</h3>
         </div>
-    )
+        {/* <img src=""></img> */}
+        <div className="open-button">
+          <div>
+            <IconButton
+              href="https://xenodochial-curie-f10544.netlify.app"
+              target={"_blank"}
+            >
+              <OpenInNewIcon color="warning" fontSize="large" />
+            </IconButton>
+          </div>
+          <div>
+            <IconButton
+              href="https://github.com/vrushabhmungelwar/forgot-password-frontend.git"
+              target={"_blank"}
+            >
+              <GitHubIcon color="warning" fontSize="large" />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
