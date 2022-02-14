dd309e235ae5240977d867cb376c0ac41579f34e
Vrushabh Mungelwar <vrushabhmungelwar10@gmail.com>
2022-02-15 00:41:22 +0530

    initial commit

--- src/projects.js
+++ src/projects.js
@@ -33,6 +33,34 @@ export function Projects() {
           </div>
         </div>
       </div>
+
+      <div className="projects">
+        <div className="proj">
+          <h3>Diary Manager</h3>
+          <p>A website which stores upcomming events and Daily Diary</p>
+          <h3>Technologies Used:</h3>
+          <p>React.js, Node.js, Express.js, Mongodb</p>
+        </div>
+        {/* <img src=""></img> */}
+        <div className="open-button">
+          <div>
+            <IconButton
+              href="https://gifted-sinoussi-8c14db.netlify.app"
+              target={"_blank"}
+            >
+              <OpenInNewIcon color="warning" fontSize="large" />
+            </IconButton>
+          </div>
+          <div>
+            <IconButton
+              href="https://github.com/vrushabhmungelwar/diary-manager.git"
+              target={"_blank"}
+            >
+              <GitHubIcon color="warning" fontSize="large" />
+            </IconButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
